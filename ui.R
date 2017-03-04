3475fbe7e6cac009a10a437b743246393b69c66a
farhanrahim42 <farhan.rahim@tm.com.my>
2017-03-04 12:31:11 +0800

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,29 @@
+
+# Rely on the 'WorldPhones' dataset in the datasets
+# package (which generally comes preloaded).
+library(datasets)
+
+# Use a fluid Bootstrap layout
+fluidPage(    
+  
+  # Give the page a title
+  titlePanel("Computer by region"),
+  
+  # Generate a row with a sidebar
+  sidebarLayout(      
+    
+    # Define the sidebar with one input
+    sidebarPanel(
+      selectInput("region", "Region:", 
+                  choices=colnames(WorldPhones)),
+      hr(),
+      helpText("Data from Microsoft The World's Computers.")
+    ),
+    
+    # Create a spot for the barplot
+    mainPanel(
+      plotOutput("phonePlot")  
+    )
+    
+  )
+)
