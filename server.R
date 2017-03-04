ddc957d385d90cef5129ad4f4b7323e8a4024758
farhanrahim42 <farhan.rahim@tm.com.my>
2017-03-04 11:45:24 +0800

    Add files via upload

---
+++ server.R
@@ -0,0 +1,17 @@
+# Rely on the 'WorldPhones' dataset in the datasets
+# package (which generally comes preloaded).
+library(datasets)
+
+# Define a server for the Shiny app
+function(input, output) {
+  
+  # Fill in the spot we created for a plot
+  output$phonePlot <- renderPlot({
+    
+    # Render a barplot
+    barplot(WorldPhones[,input$region], 
+            main=input$region,
+            ylab="Number of Computers",
+            xlab="Year")
+  })
+}
